e648a4f1f89d4f87a6a55b5d2ef528703fc46525
Florence Nightngale Kotohoyoh <nightngalenoela@gmail.com>
2021-01-15 06:45:17 +0000

    Install and connect mongoose and mongodb. create mongoose model and schema

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -22,3 +23,26 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+
+/*Database Connection*/
+let mongoose = require('mongoose')
+let uri = 'mongodb+srv://Florence:wisdom123@cluster0.77xx5.mongodb.net/Florence?retryWrites=true&w=majority'
+
+mongoose.connect(uri,
+ {useNewUrlParser: true,useUnifiedTopology: true});
+
+let urlSchema = new mongoose.Schema({
+original : {type: String, required: true},
+short: Number
+})
+
+let Url = mongoose.model('Url', urlSchema)
+
+let responseObject = {}
+let bodyParser = require('body-parser')
+app.post('api/shorturl/new', app.use(bodyParser.urlencoded({ extended: false })), (request, response) => {
+
+
+response.json(responseObject)
+})
